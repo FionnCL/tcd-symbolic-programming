45c724d1ce220077af89aab35592d49f629a4879
FionnCL <103070351+FionnCL@users.noreply.github.com>
2023-12-01 18:35:24 +0000

    Update 2020.pl

--- past-exam-papers/2020.pl
+++ past-exam-papers/2020.pl
@@ -1,3 +1,6 @@
+% IMPORTANT NOTE:
+% THIS EXAM WAS TAKE HOME, HENCE THE LONG "SHORT" QUESTIONS.
+% ---------------------------------------------
 % Q1
 % (a) (i)
 'Pat is happy every day it is sunny'.
@@ -19,10 +22,29 @@ happy(pat, X) :- sunnyDay(X).
 % (a) (v)
 day(sunny).
 pat(happy) :- day(sunny).
-% pat(What).
+% Query5 = pat(What).
 
 % (b)
-% INCOMPLETE
-sublist(Sub,List) :- 
+% INCOMPLETE <-----------------------------------
+subl(Sub,List) :- 
     append(_,Suffix,List),
-    append(sub,_,Suffix).
+    append(Sub,_,Suffix).
+
+% (c)
+nonRep([]).
+nonRep([H|T]):-
+    nonRepeating(H,T),
+    nonRep(T).
+
+nonRepeating(_,[]).
+nonRepeating(Element, [H|T]):-
+    Element\=H,
+    nonRepeating(Element, T).
+
+% (d)
+% It is not included in the answer as findall/3 attempts to find all X such that
+% It can find the subset of X in the list of [1]. 
+% The list [1] only contains one element: 1, so you would think this would be the answer but:
+% subset/2, as seen in the question, will return true for subset([],[1]), which means that
+% it will return [] as the subset of [1], it will then pattern match this with the X in findall/3,
+% resulting in the answer being [[]]!
