1f0788a23dc581a30459e935f5dbbca42e0c4597
FionnCL <103070351+FionnCL@users.noreply.github.com>
2023-12-04 20:24:51 +0000

    Create 2021.pl

---
+++ past-exam-papers/2021.pl
@@ -0,0 +1,17 @@
+% Q1
+% (a) (i)
+% This is a list. Its members are 0, followed by X.
+
+% (a) (ii)
+% This is a list, similarly to (a)(i), which members are 0, followed by Y.
+
+% (b)
+% Incomplete: Does not work for queries like membe(X, [a,b,a,a,b,c,c], 2).
+membe(_,[],0).
+membe(Item, [HL|HT], NewCount):-
+    Item=HL,
+    membe(Item, HT, Count),
+    NewCount is Count + 1.
+membe(Item, [HL|HT], Count):-
+    Item\=HL,
+    membe(Item, HT, Count).
